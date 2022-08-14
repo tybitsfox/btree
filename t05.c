4291d33e659dbd40cdf7707f5a0707639b1e6688
tybitsfox <tyyyyt@163.com>
2022-08-14 16:03:26 +0800

    2022-8-14精简、删除测试代码

--- t05.c
+++ t05.c
@@ -21,20 +21,18 @@ typedef struct _TREE
 	unsigned short rd,rm;
 } _TR;
 
-_TR		*root=NULL,*last=NULL,*deep_last[3];
+_TR		*root=NULL,*last=NULL;
 _TR		**s=NULL;
 int count=0;//number of all nodes;use for calc normally deep
-int cc=0,zz=0;
+int cc=0,zz=0; //for test
 int lim[40];
 int idx=0;
 
 int tree_sort_list(_TR *t,_TR **save,int cnt);//traverse by value's size(only low to high)
 int tree_balance();//balance by pointer
 int tree_ins(_TR *t,int i);//created new node
-int tree_b_mov();	//balance by value
-int tree_v_mov();	//for test
-int tree_bb_mov();	//test for tree_b_mov
-int t_b_mov();
+int tree_b_mov();	//balance by value,from root to leaf
+int tree_v_mov();	//balance by value,from new leaf to root
 //{{{int main(int argc,char **argv)
 int main(int argc,char **argv)
 {
@@ -48,7 +46,6 @@ int main(int argc,char **argv)
 		if((k <= 1) || k >= 33554432) //2^25
 			k=MINYCNT;
 	}
-	deep_last[0]=deep_last[1]=deep_last[2]=NULL;
 	unsigned char *p=malloc(sizeof(_TR)*(k+EXTBUF));
 	unsigned char *q=malloc(sizeof(_TR*)*(k+EXTBUF));
 	if(p == NULL)
@@ -71,49 +68,23 @@ int main(int argc,char **argv)
 			continue;
 		if(count > lim[idx])
 			idx++;
-		/*if(tree_balance())
+		/*if(tree_balance()) //adjust by value
 		{
 			l=tree_v_mov(); //此时last有效
 			if(l)
 				break;
 		}*/
 		tree_balance();
-		/*l=tree_sort_list(root,s,count);
-		for(i=0;i<l;i++)
-		{
-			printf("%d\t",s[i]->vol);
-		}
-		printf("\n--end--\n");*/
-		l=t_b_mov();
-		if(l)
-			break;
-		//i=tree_sort_list(root,s,count);
+		l=tree_b_mov();
 		t++;
 	}
-	/*if(l)
-	{
-		k=open("aaa.txt",O_RDWR);
-		if(k>0)
-		{
-			for(j=0;j<i;j++)
-			{
-				memset(ch,0,1024);
-				snprintf(ch,1024,"<v=%d;t=%p;l=%p;r=%p>",s[j]->vol,s[j]->top,s[j]->left,s[j]->right);
-				write(k,ch,strlen(ch));
-			}
-			memset(ch,0,1024);
-			snprintf(ch,1024,"\n\n<top->vol=%d\tm1->vol=%d\tm2->vol=%d\n>",deep_last[0]->vol,deep_last[1]->vol,deep_last[2]->vol);
-			write(k,ch,strlen(ch));
-			close(k);
-		}
-	}*/
 	printf("ldmax=%d\tldmin=%d\trdmax=%d\trdmin=%d\tcount=%d\tdeep=%d\n",root->ld,root->lm,root->rd,root->rm,count,idx+1);
 	i=tree_sort_list(root,s,count);
-	printf("list count=%d\tmoved times=%d\tbig move=%d\treturn=%d\n",i,cc,zz,l);
+	printf("list count=%d\tmoved times=%d\tbig move=%d\n",i,cc,zz);
 	free(p);
 	free(q);
 	printf("\n");
-	return l;
+	return 0;
 };
 //}}}
 //{{{int tree_sort_list(_TR *t,_TR **save,int cnt)
@@ -314,145 +285,6 @@ int tree_ins(_TR *t,int i)
 	return 0;
 };
 //}}}
-//{{{int tree_b_mov()
-/*
-   测试移动节点内的vol实现d，m差值大于等于2的情形,但是这个函数到后期可能会移动大量的数据。是大批量数据插入的一个效率瓶颈。
-   因此，该函数中有个阀值，可以限定两个待移动数据之间的距离，这样可以极大的提高插入效率，可是这样生成的二叉树并不完美，某些情况下
-   可以先快速插入，待完成后再关闭阀值，通过该函数逐步调整至完美状态。
-   返回值：0 正常返回，否则错误。
- */
-int tree_b_mov()
-{
-	_TR *c,*t,*c1,*max,*min;
-	int i,j,k,l;
-	c=root;t=NULL;
-	while(c != NULL)
-	{
-		i=c->ld-c->lm;j=c->rd-c->rm;
-		if(i>=2)
-		{t=c;c=c->left;continue;}
-		if(j>=2)
-		{t=c;c=c->right;continue;}
-		break;
-	}//t is root's top for adjusted,or error
-	if(t == NULL)
-	{
-		c=root;
-		if(c != NULL)
-		{
-			i=c->ld>c->rd?(c->ld):(c->rd);
-			j=c->lm>c->rm?c->rm:c->lm;
-			if(i<(j+2))
-				return 0;
-		}
-		zz++;
-	}
-	else
-	{
-		if((t->ld-t->lm) >= 2)
-			c=t->left; //root,到这里，最大和最小已经在不同的分支上了
-		else
-		{
-			if((t->rd-t->rm) >= 2)
-				c=t->right;
-			else
-			{printf("error001\n");return 1;}
-		}
-	}
-	/*i=c->ld>=c->rd?c->ld:c->rd; //阀值！！
-	if(i>10) //超过6层不调整了，即调整范围是128个节点内,1024
-		return 0;*/
-	c1=c;
-	while(c != NULL)
-	{
-		max=c;
-		if(c->ld >= c->rd)
-			c=c->left;
-		else
-			c=c->right;
-	}
-	c=c1;
-	while(c != NULL)
-	{
-		min=c;
-		if(c->lm >= c->rm)
-			c=c->right;
-		else
-			c=c->left;
-	}
-	memset((void*)s,0,sizeof(_TR*)*(count+3));
-	j=tree_sort_list(c1,s,count);//这里得到的是子树c1的节点数组，需要调整的只是min到max之间的节点
-	if((max->ld != 1) || (max->rd != 1))//verified 
-		return 0;
-	if((min->lm != 1) && (min->rm != 1))
-		return 0;
-	if(max->vol < min->vol) //从左至右调整
-	{
-		k=max->vol;t=max->top;
-		if(t->left == max)
-		{t->left=NULL;t->ld=t->lm=1;}
-		else
-		{t->right=NULL;t->rd=t->rm=1;}
-		for(i=0;i<(j-1);i++)
-		{
-			if(s[i]->vol <= max->vol)
-				continue;
-			l=s[i]->vol;s[i]->vol=k;k=l;
-			if(s[i+1]->vol >= min->vol)
-			{
-				if(s[i+1]->vol > min->vol)
-					break;
-				if(min->left == NULL)
-				{min->left=max;max->top=min;max->vol=k;}
-				else
-				{min->right=max;max->top=min;max->vol=min->vol;min->vol=k;}
-				break;
-			}
-		}//开始调整层数：t，max
-	}
-	else //从右至左调整
-	{
-		k=max->vol;t=max->top;
-		if(t->left == max)
-		{t->left=NULL;t->ld=t->lm=1;}
-		else
-		{t->right=NULL;t->rd=t->rm=1;}
-		for(i=(j-1);i>0;i--)
-		{
-			if(s[i]->vol >= max->vol)
-				continue;
-			l=s[i]->vol;s[i]->vol=k;k=l;
-			if(s[i-1]->vol <= min->vol)
-			{
-				if(s[i-1]->vol < min->vol)
-					break;
-				if(min->right == NULL)
-				{min->right=max;max->top=min;max->vol=k;}
-				else
-				{min->left=max;max->top=min;max->vol=min->vol;min->vol=k;}
-				break;
-			}
-		}
-	}
-	k=0;
-	while(k<=1) //调整两个子树的层数值
-	{
-		while(t->top != NULL)
-		{
-			i=t->ld>=t->rd?(t->ld+1):(t->rd+1);
-			j=t->lm>=t->rm?(t->rm+1):(t->lm+1);
-			c=t;t=t->top;
-			if(t->left==c)
-			{t->ld=i;t->lm=j;}
-			else
-			{t->rd=i;t->rm=j;}
-		}
-		t=max;k++;
-	}
-	cc++;
-	return 0;
-};
-//}}}
 //{{{int tree_v_mov()
 /*这是一个自叶向根的平衡移动，好处是尽量减少每次移动的数据量，但移动次数要多*/
 int tree_v_mov()
@@ -465,8 +297,6 @@ int tree_v_mov()
 	while(c->top != NULL)
 	{
 		t=c;c=c->top;k++;
-//		if(k>10)
-//			return 0;
 		if(c->left == t)//左返回
 		{
 			if(c->ld < (c->rm+2))
@@ -572,139 +402,9 @@ int tree_v_mov()
 	return 0;
 };
 //}}}
-//{{{int tree_bb_mov()
-/*虽然这种方法不完美，但继续测试下。画个比较满意的句号吧*/
-int tree_bb_mov()
-{
-	_TR *c,*c1,*t,*m1,*m2;
-	int i,j,k,l,v1,v2;
-	c=root;t=NULL;k=0;
-	while(c != NULL)
-	{
-		i=c->ld >= c->rd?c->ld:c->rd;
-		j=c->lm >= c->rm?c->rm:c->lm;
-		if(i < (j+2))
-			break;
-		t=c;
-		if(c->lm >= c->rd)
-			c=c->right;
-		else
-			c=c->left;
-	}
-	if(t == NULL)
-		return 0;
-	m1=m2=NULL;c=t;
-	while(c != NULL)
-	{
-		m1=c;
-		if(c->ld > c->rd)
-			c=c->left;
-		else
-			c=c->right;
-	}
-	c=t;
-	while(c != NULL)
-	{
-		m2=c;
-		if(c->lm >= c->rm)
-			c=c->right;
-		else
-			c=c->left;
-	}
-	if(m1 == NULL || m2 == NULL)
-	{printf("error001\n");return 1;}
-	memset((void*)s,0,sizeof(_TR*)*(count+3));
-	j=tree_sort_list(t,s,count);
-	v1=v2=-1;
-	for(i=0;i<j;i++)
-	{
-		if(s[i] == m1)
-		{v1=i;break;}
-	}
-	for(i=0;i<j;i++)
-	{
-		if(s[i] == m2)
-		{v2=i;break;}
-	}
-	if((v1 == -1) || (v2 == -1))
-	{printf("error002\n");return 1;}
-	if(v1 == v2)
-	{printf("error003\n");return 1;}
-	k=m1->vol;c=m1->top;
-	if(c->left == m1)
-	{c->left=NULL;c->ld=c->lm=1;}
-	else
-	{
-		if(c->right == m1)
-		{c->right=NULL;c->rd=c->rm=1;}
-		else
-		{printf("error004\n");return 1;}
-	}
-	if(v1 < v2)
-	{
-		for(i=(v1+1);i<v2;i++)
-		{l=s[i]->vol;s[i]->vol=k;k=l;}
-		if(m2->left == NULL)
-		{m2->left=m1;m1->top=m2;m1->vol=k;}
-		else
-		{
-			if(m2->right == NULL)
-			{m2->right=m1;m1->top=m2;m1->vol=m2->vol;m2->vol=k;}
-			else
-			{printf("error005\n");return 1;}
-		}
-	}
-	else
-	{
-		for(i=(v1-1);i>v2;i--)
-		{l=s[i]->vol;s[i]->vol=k;k=l;}
-		if(m2->right == NULL)
-		{m2->right=m1;m1->top=m2;m1->vol=k;}
-		else
-		{
-			if(m2->left == NULL)
-			{m2->left=m1;m1->top=m2;m1->vol=m2->vol;m2->vol=k;}
-			else
-			{printf("error006\n");return 1;}
-		}
-	}
-	while(c->top != NULL)
-	{
-		i=c->ld >= c->rd ?(c->ld+1):(c->rd+1);
-		j=c->lm >= c->rm ?(c->rm+1):(c->lm+1);
-		t=c;c=c->top;
-		if(t == c->left)
-		{c->ld=i;c->lm=j;}
-		else
-		{
-			if(t == c->right)
-			{c->rd=i;c->rm=j;}
-			else
-			{printf("error007\n");return 1;}
-		}
-	}
-	while(m1->top != NULL)
-	{
-		i=m1->ld >= m1->rd ?(m1->ld+1):(m1->rd+1);
-		j=m1->lm >= m1->rm ?(m1->rm+1):(m1->lm+1);
-		t=m1;m1=m1->top;
-		if(t == m1->left)
-		{m1->ld=i;m1->lm=j;}
-		else
-		{
-			if(t == m1->right)
-			{m1->rd=i;m1->rm=j;}
-			else
-			{printf("error008\n");return 1;}
-		}
-	}
-	cc++;
-	return 0;
-};
-//}}}
-//{{{int t_b_mov()
+//{{{int tree_b_mov()
 /*继续测试，ld-lm or rd-rm >=2的情形出错的原因*/
-int t_b_mov()
+int tree_b_mov()
 {
 	_TR *c,*t,*m1,*m2;
 	int i,j,k,v1,v2;
@@ -756,18 +456,12 @@ int t_b_mov()
 		if(s[i] == m1)
 		{v1=i;break;}
 	}
-	if(v1 == -1)
-		return 32;
 	for(i=0;i<j;i++)
 	{
 		if(s[i] == m2)
 		{v2=i;break;}
 	}
-	if(v2 == -1)
-		return 33;
 	k=m1->vol;t=m1->top;
-	if(t == NULL)
-	{return 4;}
 	if(t->left == m1)
 	{t->left=NULL;t->ld=t->lm=1;}
 	else
